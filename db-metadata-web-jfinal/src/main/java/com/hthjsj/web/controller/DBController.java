7829a0dce622d26f4796dd38d6749f8d8385241a
erimer <konbluesky@163.com>
2019-10-28 11:42:13 +0800

    update;

--- db-metadata-web-jfinal/src/main/java/com/hthjsj/web/controller/DBController.java
+++ db-metadata-web-jfinal/src/main/java/com/hthjsj/web/controller/DBController.java
@@ -1,12 +1,14 @@
 package com.hthjsj.web.controller;
 
 import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
 import com.hthjsj.analysis.db.MysqlService;
+import com.hthjsj.analysis.db.Table;
 import com.jfinal.aop.Aop;
+import com.jfinal.kit.Kv;
 import com.jfinal.kit.Ret;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * <p> @Date : 2019/10/9 </p>
@@ -36,8 +38,13 @@ public class DBController extends FrontRestController {
     public void tables() {
         String schemaName = getPara(0, getPara("schemaName"));
         Preconditions.checkNotNull(schemaName, "[schemaName]数据库名称是必填参数");
-        List<String> tableNames = Aop.get(MysqlService.class).showTables(schemaName).stream().map(table -> table.getTableName()).collect(Collectors.toList());
+        List<Table> tables = Aop.get(MysqlService.class).showTables(schemaName);
+        List<Kv> results = Lists.newArrayList();
+        tables.forEach(r -> {
+            results.add(Kv.create().set("key", r.getTableName()).set("value", r.getTableName()));
+        });
+        //        List<String> tableNames = Aop.get(MysqlService.class).showTables(schemaName).stream().map(table -> table.getTableName()).collect(Collectors.toList());
 
-        renderJson(Ret.ok("data", tableNames));
+        renderJson(Ret.ok("data", results));
     }
 }
