96cf3f3fa6fe818a8589d83ec88e3123f474d0a0
erimer <konbluesky@163.com>
2019-10-31 15:18:02 +0800

    add dict 男女

--- db-metadata-web-jfinal/src/main/java/com/hthjsj/web/controller/DictController.java
+++ db-metadata-web-jfinal/src/main/java/com/hthjsj/web/controller/DictController.java
@@ -1,8 +1,13 @@
 package com.hthjsj.web.controller;
 
+import com.google.common.collect.Lists;
 import com.hthjsj.analysis.meta.MetaObject;
 import com.hthjsj.web.ServiceManager;
 import com.hthjsj.web.query.QueryHelper;
+import com.jfinal.kit.Kv;
+import com.jfinal.kit.Ret;
+
+import java.util.List;
 
 /**
  * <p> Class title: </p>
@@ -22,5 +27,12 @@ public class DictController extends FrontRestController {
 
 
         MetaObject metaObject = (MetaObject) ServiceManager.dbMetaService().findByCode(objectCode);
+    }
+
+    public void dict() {
+        List<Kv> options = Lists.newArrayList();
+        options.add(Kv.by("key", "男").set("value", "1"));
+        options.add(Kv.by("key", "女").set("value", "2"));
+        renderJson(Ret.ok("data", options));
     }
 }
