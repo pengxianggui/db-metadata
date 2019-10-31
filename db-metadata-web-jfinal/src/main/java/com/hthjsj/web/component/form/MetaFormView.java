7fe7b194faa97295fc0f1898a9504309e47ce76c
pengxianggui <pengxianggui@outlook.com>
2019-10-31 14:41:07 +0800

    暂时屏蔽

--- db-metadata-web-jfinal/src/main/java/com/hthjsj/web/component/form/MetaFormView.java
+++ db-metadata-web-jfinal/src/main/java/com/hthjsj/web/component/form/MetaFormView.java
@@ -42,10 +42,10 @@ public class MetaFormView extends FormView {
         for (IMetaField metaField : metaObject.fields()) {
             config = JSON.parseObject(fieldsConfig.getStr(metaField.fieldCode()), Kv.class);
             FormField formField = FormFieldFactory.createFormField(metaField, config);
-            AccessBehavior accessBehavior = formField.getAccessBehavior();
-            //            if (accessBehavior.isAdd()) {
-            //                fields.add(formField);
-            //            }
+//            AccessBehavior accessBehavior = formField.getAccessBehavior();
+//            if (accessBehavior.isAdd()) {
+//                fields.add(formField);
+//            }
 
             fields.add(formField);
         }
