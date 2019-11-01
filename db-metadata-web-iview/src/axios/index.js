1a1dd14bb8273d40b6c4ec9791cb7f830057d19b
pengxianggui <pengxianggui@outlook.com>
2019-11-01 15:55:36 +0800

    移除axios对post请求生效的全局配置的qs.stringrify

--- db-metadata-web-iview/src/axios/index.js
+++ db-metadata-web-iview/src/axios/index.js
@@ -1,17 +1,13 @@
 import axios from 'axios'
-import qs from 'qs'
 
 // import Qs from 'qs' // 用来处理参数，可不使用，若要使用，npm安装： npm install qs
 axios.defaults.baseURL = '/'; // 请求的默认域名
-axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=utf-8';
+axios.defaults.headers.post['Content-Type'] = 'application/json; charset=utf-8';
 
 // 添加一个请求拦截器
 axios.interceptors.request.use(config => {
         // config.headers.languagetype = 'CN' // 举例，加上一个公共头部
         // config.data = Qs.stringify(config.data) // 处理数据，可不写
-        if (config.method.toLowerCase() === 'post') {
-            config.data = qs.stringify(config.data)
-        }
         return config
     },
     err => {
