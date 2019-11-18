577eb624142ce2000130b8e96118d42d4d8fcd28
pengxianggui <pengxianggui@outlook.com>
2019-11-18 15:16:39 +0800

    update

--- db-metadata-web-iview/src/components/core/FormTmpl.vue
+++ db-metadata-web-iview/src/components/core/FormTmpl.vue
@@ -31,7 +31,7 @@
         name: "FormTmpl",
         data() {
             return {
-                // model: {},
+                model: {},
                 isEdit: false
             }
         },
@@ -77,28 +77,44 @@
                     this.$emit('cancel', event);
                     return;
                 }
+            },
+            assemblyModel(meta) {
+                this.model = {};
+                let columns = meta.columns;
+                let record = meta.hasOwnProperty('record') ? meta.record : {};
+                this.isEdit = record.hasOwnProperty('id') && (record.id != null);
+
+                columns.forEach(item => {
+                    this.$merge(item, DEFAULT[item.component_name]); // merge column
+                    this.$set(this.model, item.name, record[item.name] || item.default_value);
+                });
             }
         },
         computed: {
             innerMeta() {
                 let meta = this.$merge(this.meta, DEFAULT.FormTmpl);
-                let columns = meta.columns;
-                for (let i = 0; i < columns.length; i++) {
-                    let item = columns[i];
-                    this.$merge(item, DEFAULT[item.component_name]); // merge column
-                }
+                this.assemblyModel(meta);
                 return meta;
             },
-            model() { // rely on innerMeta
-                let model = {};
-                let record = this.innerMeta.hasOwnProperty('record') ? this.innerMeta.record : {};
-                let columns = this.innerMeta.columns;
-                for (let i = 0; i < columns.length; i++) {
-                    let item = columns[i];
-                    this.$set(model, item.name, record[item.name] || item.default_value);
-                }
-                return model;
-            }
+            // innerMeta() {
+            //     let meta = this.$merge(this.meta, DEFAULT.FormTmpl);
+            //     let columns = meta.columns;
+            //     for (let i = 0; i < columns.length; i++) {
+            //         let item = columns[i];
+            //         this.$merge(item, DEFAULT[item.component_name]); // merge column
+            //     }
+            //     return meta;
+            // },
+            // model() { // rely on innerMeta
+            //     let model = {};
+            //     let record = this.innerMeta.hasOwnProperty('record') ? this.innerMeta.record : {};
+            //     let columns = this.innerMeta.columns;
+            //     for (let i = 0; i < columns.length; i++) {
+            //         let item = columns[i];
+            //         this.$set(model, item.name, record[item.name] || item.default_value);
+            //     }
+            //     return model;
+            // }
         }
     }
 </script>
