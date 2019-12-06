fce17aee11b6a0f09647f6636a49f783f4ba18b2
pengxianggui <pengxianggui@outlook.com>
2019-12-06 09:05:33 +0800

    fix bug: 左侧菜单收缩时, 切换模块导致自动再次打开

--- db-metadata-web-iview/src/App.vue
+++ db-metadata-web-iview/src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="app">
-        <router-view :key="new Date().getTime()"></router-view>
+        <router-view></router-view>
     </div>
 </template>
 
--- db-metadata-web-iview/src/components/layout/AsideNav.vue
+++ db-metadata-web-iview/src/components/layout/AsideNav.vue
@@ -78,10 +78,10 @@
         },
         methods: {
             handleOpen(key, keyPath) {
-                console.log(key, keyPath);
+                // console.log(key, keyPath);
             },
             handleClose(key, keyPath) {
-                console.log(key, keyPath);
+                // console.log(key, keyPath);
             }
         }
     }
