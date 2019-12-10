f715d35bbe232c9677caf73a49eb0f218ee5c3a7
erimer <konbluesky@163.com>
2019-12-10 16:59:58 +0800

    update;

--- db-metadata-web-jfinal/src/main/java/com/hthjsj/web/feature/ms/MasterSlaveController.java
+++ db-metadata-web-jfinal/src/main/java/com/hthjsj/web/feature/ms/MasterSlaveController.java
@@ -2,31 +2,22 @@ package com.hthjsj.web.feature.ms;
 
 import com.google.common.base.Preconditions;
 import com.hthjsj.analysis.meta.IMetaObject;
-import com.hthjsj.analysis.meta.MetaData;
-import com.hthjsj.analysis.meta.MetaObjectConfigParse;
-import com.hthjsj.analysis.meta.aop.AddPointCut;
-import com.hthjsj.analysis.meta.aop.AopInvocation;
 import com.hthjsj.web.ServiceManager;
 import com.hthjsj.web.component.ViewFactory;
 import com.hthjsj.web.component.form.FormView;
-import com.hthjsj.web.query.FormDataFactory;
 import com.hthjsj.web.query.QueryHelper;
 import com.jfinal.core.Controller;
 import com.jfinal.kit.Ret;
-import com.jfinal.plugin.activerecord.Db;
-import com.jfinal.plugin.activerecord.IAtom;
 import lombok.extern.slf4j.Slf4j;
-
-import java.sql.SQLException;
 
 /**
  * <pre>
  * 主子表作为功能存在
  *
  *  1. 主对象
- *      1.1 增加
- *      1.2 修改
- *      1.3 删除 (级联,删除子对象)
+ *      1.1 增加 (无特殊,可用Form)
+ *      1.2 修改 (无特殊,可用Form)
+ *      1.3 删除 (!!级联,删除子对象)
  *  2. 子对象
  *      > 1 主 -> N 子对象
  *      2.1 增加子对象时 关联主对象
@@ -55,50 +46,6 @@ import java.sql.SQLException;
  */
 @Slf4j
 public class MasterSlaveController extends Controller {
-
-    public void toAddM() {
-        QueryHelper queryHelper = new QueryHelper(this);
-        String objectCode = queryHelper.getObjectCode();
-
-        IMetaObject metaObject = ServiceManager.metaService().findByCode(objectCode);
-
-        FormView formView = ViewFactory.formView(metaObject).action("/form/doAdd").addForm();
-
-        renderJson(Ret.ok("data", formView.toKv()));
-    }
-
-    public void doAddM() {
-        QueryHelper queryHelper = new QueryHelper(this);
-        String objectCode = queryHelper.getObjectCode();
-
-        IMetaObject metaObject = ServiceManager.metaService().findByCode(objectCode);
-
-        MetaData metadata = FormDataFactory.buildFormData(getRequest().getParameterMap(), metaObject, true);
-
-        MetaObjectConfigParse metaObjectConfigParse = metaObject.configParser();
-        AddPointCut pointCut = metaObjectConfigParse.addPointCut();
-        AopInvocation invocation = new AopInvocation(metaObject, metadata, getKv());
-
-        boolean status = Db.tx(new IAtom() {
-
-            @Override
-            public boolean run() throws SQLException {
-                boolean s = false;
-                try {
-                    pointCut.addBefore(invocation);
-                    s = ServiceManager.metaService().saveData(invocation.getMetaObject(), invocation.getFormData());
-                    pointCut.addAfter(s, invocation);
-                } catch (Exception e) {
-                    log.error("保存异常\n元对象:{},错误信息:{}", metaObject.code(), e.getMessage());
-                    log.error(e.getMessage(), e);
-                    s = false;
-                }
-                return s;
-            }
-        });
-
-        renderJson(status ? Ret.ok() : Ret.fail());
-    }
 
     /**
      * ?featureCode=&objectCode=
