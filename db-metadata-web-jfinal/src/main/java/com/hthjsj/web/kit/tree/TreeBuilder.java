4f889263b3e8791638264ee40c534e02cb375f1d
erimer <konbluesky@163.com>
2019-12-18 16:36:23 +0800

    update;

--- db-metadata-web-jfinal/src/main/java/com/hthjsj/web/kit/tree/TreeBuilder.java
+++ db-metadata-web-jfinal/src/main/java/com/hthjsj/web/kit/tree/TreeBuilder.java
@@ -1,6 +1,5 @@
 package com.hthjsj.web.kit.tree;
 
-import com.alibaba.fastjson.JSON;
 import com.jfinal.kit.StrKit;
 import lombok.extern.slf4j.Slf4j;
 
@@ -17,25 +16,6 @@ import java.util.*;
 public class TreeBuilder {
 
     List<TreeNode> returnList = new ArrayList<TreeNode>();
-
-    public static void main(String[] args) {
-        Note n1 = new Note(1, 0, "根节点");
-        Note n2 = new Note(2, 1, "1节点");
-        Note n3 = new Note(3, 1, "2节点");
-        Note n4 = new Note(4, 3, "3节点");
-        Note n5 = new Note(5, 3, "4节点");
-        Note n6 = new Note(6, 2, "5节点");
-        List list = new ArrayList();
-        list.add(n1);
-        list.add(n2);
-        list.add(n3);
-        list.add(n4);
-        list.add(n5);
-        list.add(n6);
-        TreeBuilder tu = new TreeBuilder();
-        List<Note> result = tu.getChildTreeObjects(list, Integer.valueOf(0));
-        System.out.println(JSON.toJSONString(result));
-    }
 
     /**
      * 判断两个父ID是否相同
@@ -227,67 +207,5 @@ public class TreeBuilder {
      */
     private boolean hasChild(List<TreeNode> list, TreeNode t) {
         return getChildList(list, t).size() > 0;
-    }
-}
-
-class Note implements TreeNode<Integer, Object> {
-
-    private int id;
-
-    private int parentid;
-
-    private String name;
-
-    private List children;
-
-    public Note(int id, int parentid, String name) {
-        this.id = id;
-        this.parentid = parentid;
-        this.name = name;
-    }
-
-    @Override
-    public Integer getId() {
-        return id;
-    }
-
-    @Override
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    @Override
-    public Integer getParentId() {
-        return parentid;
-    }
-
-    @Override
-    public void setParentId(Integer parentId) {
-        this.parentid = parentId;
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public List getChildren() {
-        return children;
-    }
-
-    @Override
-    public void setChildren(List children) {
-        this.children = children;
-    }
-
-    @Override
-    public Object currNode() {
-        return null;
     }
 }
