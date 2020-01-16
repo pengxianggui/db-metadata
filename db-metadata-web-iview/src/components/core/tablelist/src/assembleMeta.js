893d448cd2ef07808772203ca6bd7cf07b5c37fa
pengxianggui <pengxianggui@outlook.com>
2020-01-16 16:06:01 +0800

    update

--- db-metadata-web-iview/src/components/core/tablelist/src/assembleMeta.js
+++ db-metadata-web-iview/src/components/core/tablelist/src/assembleMeta.js
@@ -14,12 +14,5 @@ export default function (mergedMeta) {
     if (utils.hasProp(mergedMeta, columnsKey) && utils.isArray(mergedMeta[columnsKey])) {
         initShowable(mergedMeta[columnsKey]);
     }
-    const {data_url, delete_url, objectCode} = mergedMeta;
-
-    // 当服务端meta缺少data_url或delete_url时, 会使用系统默认的data_url和delete_url, 系统默认的需要进行编译(objectCode是动态的)
-    if (!utils.isEmpty(objectCode)) {
-        mergedMeta['data_url'] = utils.compile(data_url, {objectCode: objectCode});
-        mergedMeta['delete_url'] = utils.compile(delete_url, {objectCode: objectCode});
-    }
     return mergedMeta;
 }
