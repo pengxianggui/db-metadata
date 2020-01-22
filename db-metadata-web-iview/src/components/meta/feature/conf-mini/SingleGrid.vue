2da3f0a8dc1c61ec4bc1ce363a255ea1fcd1b62c
pengxianggui <pengxianggui@outlook.com>
2020-01-22 11:28:47 +0800

    update

---
+++ db-metadata-web-iview/src/components/meta/feature/conf-mini/SingleGrid.vue
@@ -0,0 +1,13 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+    export default {
+        name: "SingleGrid"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ db-metadata-web-iview/src/components/meta/feature/conf-mini/index.js
@@ -0,0 +1,3 @@
+import SingleGrid from './SingleGrid'
+import MasterSlaveGrid from './MasterSlaveGrid'
+import Vue from 'vue'
