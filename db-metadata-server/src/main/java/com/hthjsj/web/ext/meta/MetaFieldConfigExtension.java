208fe234f0834b956ee97cab2aa75adf97e0b7b9
konbluesky <konbluesky@163.com>
2020-07-30 11:09:10 +0800

    url rename;

--- db-metadata-server/src/main/java/com/hthjsj/web/ext/meta/MetaFieldConfigExtension.java
+++ db-metadata-server/src/main/java/com/hthjsj/web/ext/meta/MetaFieldConfigExtension.java
@@ -1,10 +1,8 @@
 package com.hthjsj.web.ext.meta;
 
-import com.alibaba.fastjson.JSON;
 import com.hthjsj.analysis.component.ComponentType;
 import com.hthjsj.analysis.meta.ConfigExtension;
 import com.hthjsj.analysis.meta.IMetaField;
-import com.hthjsj.analysis.meta.MetaFieldConfigParse;
 import com.jfinal.kit.Kv;
 
 /**
@@ -21,8 +19,7 @@ public class MetaFieldConfigExtension implements ConfigExtension<IMetaField, Kv,
         //TODO 初始化field配置时 , 名称包含file的 默认设定isFile = true
         if (metaField.fieldCode().contains("file")) {
             config.set("isFile", true);
-            MetaFieldConfigParse.FileConfig fileConfig = new MetaFieldConfigParse.FileConfig();
-            config.set("fileConfig", JSON.toJSONString(fileConfig));
+            config.set("seats", "[]");
         }
     }
 }
