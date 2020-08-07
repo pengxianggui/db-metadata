a47ef58e7f33f9c63b6fe44808b83fad38639f0e
konbluesky <konbluesky@163.com>
2020-08-07 12:38:51 +0800

    add TreeKit.java

---
+++ db-metadata-server/src/main/java/com/hthjsj/web/kit/tree/TreeKit.java
@@ -0,0 +1,34 @@
+package com.hthjsj.web.kit.tree;
+
+import com.alibaba.fastjson.serializer.AfterFilter;
+import com.hthjsj.web.WebException;
+import com.jfinal.plugin.activerecord.Record;
+
+/**
+ * <p> @Date : 2020/8/6 </p>
+ * <p> @Project : db-meta-serve</p>
+ *
+ * <p> @author konbluesky </p>
+ */
+public class TreeKit {
+
+    /* TreeNode渲染时,通过filter 将currNode内容,渲染到json 根下 */
+    public final static AfterFilter afterFilter = new AfterFilter() {
+
+        /* object为运行时传TreeNode */
+        @Override
+        public void writeAfter(Object object) {
+            if (object instanceof TreeNode) {
+                TreeNode treeNode = (TreeNode) object;
+                Object currNode = treeNode.currNode();
+                if (currNode instanceof Record) {
+                    ((Record) currNode).getColumns().forEach((key, value) -> {
+                        writeKeyValue(key, value);
+                    });
+                } else {
+                    throw new WebException("还未支持的TreeNode Json 转换");
+                }
+            }
+        }
+    };
+}
