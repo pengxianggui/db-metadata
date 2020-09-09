c6fdc83453de1d3fb3c208e77c734f0df0344350
pengxg <pengxianggui@outlook.com>
2020-09-09 14:53:17 +0800

    修复BoolBox缺陷: 当传入值为空串, 会选中

--- db-metadata-web/package/core/boolbox/src/conver.js
+++ db-metadata-web/package/core/boolbox/src/conver.js
@@ -1,5 +1,8 @@
 import expand from './expand'
 
 export default function (value) {
+    if (expand.indexOf(value) < 0) {
+        return false
+    }
     return (expand.indexOf(value) + 1) % 2 == 0;
 }
