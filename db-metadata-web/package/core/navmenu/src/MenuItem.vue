c937167be9bbe8e718c364ff6163359f6e29cc83
pengxianggui <pengxianggui@outlook.com>
2021-01-21 15:14:38 +0800

    支持快捷编辑菜单元数据

--- db-metadata-web/package/core/navmenu/src/MenuItem.vue
+++ db-metadata-web/package/core/navmenu/src/MenuItem.vue
@@ -1,91 +1,101 @@
 <template>
   <div v-if="!item.hidden &&  $hasRoles(needRoles)">
     <app-link v-if="noChild(item)" :to="item.path" :query="resolveParams(item['params'])">
-        <el-menu-item :index="item.path">
+      <pop-menu trigger="right-click" :disabled="!metaEditable">
+        <template #label>
+          <el-menu-item :index="item.path">
             <svg-icon :value="item.icon" v-if="item.icon"></svg-icon>
-            <span slot="title">{{item.title}}</span>
-        </el-menu-item>
+            <span slot="title">{{ item.title }}</span>
+          </el-menu-item>
+        </template>
+        <list>
+          <list-item @click="editMenuMeta(item)">编辑元菜单</list-item>
+        </list>
+      </pop-menu>
     </app-link>
 
     <el-submenu v-else ref="subMenu" :index="item.path" popper-append-to-body>
-        <template slot="title">
-            <svg-icon :value="item.icon" v-if="item.icon"></svg-icon>
-            <span slot="title">{{item.title}}</span>
-        </template>
-        <template v-for="subMenu in item.children">
-          <menu-item :key="subMenu.path"
-                     :item="subMenu"></menu-item>
-        </template>
+      <template slot="title">
+        <pop-menu trigger="right-click" :disabled="!metaEditable">
+          <template #label>
+            <div>
+              <svg-icon :value="item.icon" v-if="item.icon"></svg-icon>
+              <span slot="title">{{ item.title }}</span>
+            </div>
+          </template>
+          <list>
+            <list-item @click="editMenuMeta(item)">编辑元菜单</list-item>
+          </list>
+        </pop-menu>
+      </template>
+      <template v-for="subMenu in item.children">
+        <menu-item :key="subMenu.path"
+                   :item="subMenu"></menu-item>
+      </template>
     </el-submenu>
   </div>
 </template>
 
 <script>
-    import AppLink from './Link'
-    import utils from '@/../package/utils'
+import AppLink from './Link'
+import utils from '@/../package/utils'
+import {restUrl} from "../../../constant/url";
+import {isEmpty} from "../../../utils/common";
 
-    export default {
-        name: "MenuItem",
-        components: {AppLink},
-        props: {
-            // route object
-            item: {
-                type: Object,
-                required: true
-            }
-        },
-        data() {
-            return {}
-        },
-        methods: {
-            noChild(item) {
-                return !item.hasOwnProperty('children') || item['children'].length === 0;
-            },
-            // hasOneShowingChild(children = [], parent) {
-            //     // if (parent.hasOwnProperty('hidden') && parent.hidden === false) return false;
-            //
-            //     const showingChildren = children.filter(item => !item.hidden);
-            //
-            //     // if (showingChildren.length === 1) {
-            //     //     this.onlyOneChild = showingChildren[0];
-            //     //     return true
-            //     // }
-            //
-            //     if (showingChildren.length === 0) {
-            //         this.onlyOneChild = {...parent, path: '', noShowingChildren: true}
-            //         return true
-            //     }
-            //
-            //     return false
-            // },
-            // resolvePath(routePath, params) {
-            //     if (isExternal(routePath)) {
-            //         return routePath
-            //     }
-            //     if (isExternal(this.basePath)) {
-            //         return this.basePath
-            //     }
-            //     return path.resolve(this.basePath, routePath)
-            // },
-            resolveParams(pathParams) {
-                let params = {};
-                if (pathParams && pathParams.indexOf('=') > -1) {
-                    let [key, value] = pathParams.split('=');
-                    params[key] = value;
-                }
-                return params;
-            }
-        },
-        computed: {
-          needRoles() {
-            let {item: {meta}} = this
-            meta = utils.strToObject(meta)
-            if (!meta) {
-              meta = {}
-            }
-            const {roles} = meta
-            return roles
-          }
-        }
+export default {
+  name: "MenuItem",
+  components: {AppLink},
+  props: {
+    // route object
+    item: {
+      type: Object,
+      required: true
     }
+  },
+  data() {
+    return {}
+  },
+  methods: {
+    editMenuMeta(item) {
+      if (!this.$isRoot()) {
+        this.$message.error("只有开发者账号可以执行此操作")
+        return
+      }
+      const objectCode = "meta_menu" // 系统元对象编码-固定
+      const {id, name} = item
+      let url = this.$compile(restUrl.RECORD_TO_UPDATE, {objectCode: objectCode, primaryKv: id});
+      this.$axios.get(url).then(({data}) => {
+        this.$dialog(data, null, {
+          title: `编辑菜单元数据:${name}`
+        })
+      });
+    },
+    noChild(item) {
+      return !item.hasOwnProperty('children') || item['children'].length === 0;
+    },
+    resolveParams(pathParams) {
+      let params = {};
+      if (pathParams && pathParams.indexOf('=') > -1) {
+        let [key, value] = pathParams.split('=');
+        params[key] = value;
+      }
+      return params;
+    }
+  },
+  computed: {
+    needRoles() {
+      let {item: {meta}} = this
+      meta = utils.strToObject(meta)
+      if (!meta) {
+        meta = {}
+      }
+      const {roles} = meta
+      return roles
+    },
+    metaEditable() { // 是否可编辑菜单元数据
+      const {item: {id}} = this
+      return this.$isRoot() && !isEmpty(id)
+    }
+  }
+}
 </script>
--- db-metadata-web/package/core/popmenu/src/PopMenu.vue
+++ db-metadata-web/package/core/popmenu/src/PopMenu.vue
@@ -1,70 +1,68 @@
 <template>
-    <el-popover :trigger="nativeTrigger" :placement="placement" v-model="visible"
-                popper-class="no-padding" v-bind="$attrs" @show="$emit('show')">
-        <span slot="reference">
-            <span @click.right="rightClickHander">
-                <slot name="label">
-                    <i class="el-icon-caret-bottom" style="cursor: pointer"></i>
-                </slot>
-            </span>
-        </span>
-        <slot>
-            <list>
-                <template #body>
-                    <slot name="body"></slot>
-                </template>
-            </list>
+  <el-popover :trigger="nativeTrigger" :placement="placement" v-model="visible"
+              popper-class="no-padding" v-bind="$attrs" @show="$emit('show')">
+    <span slot="reference" @click.right="rightClickHander">
+        <slot name="label">
+            <i class="el-icon-caret-bottom" style="cursor: pointer"></i>
         </slot>
-    </el-popover>
+    </span>
+    <slot>
+      <list>
+        <template #body>
+          <slot name="body"></slot>
+        </template>
+      </list>
+    </slot>
+  </el-popover>
 </template>
 
 <script>
-    export default {
-        name: "PopMenu",
-        props: {
-            trigger: {
-                type: String,
-                default: () => 'click',
-                validator: value => ['click', 'focus', 'hover', 'right-click'].indexOf(value) > -1
-            },
-            placement: String,
-            // visible: {
-            //   type: Boolean,
-            //   default: () => false
-            // }
-        },
-        data() {
-            return {
-                visible: false
-            }
-        },
-        methods: {
-            rightClickHander(ev) {
-                let self = this;
-                if (self.trigger === 'right-click') {
-                    ev.preventDefault();
-                    self.visible = true;
-                    // self.$emit('update:visible', true)
-                    let div = document.createElement('div');
-                    div.style = 'position: absolute; width: 100%; height: 100%; top: 0; left: 0; z-index: 1;';
-                    document.body.appendChild(div);
-                    div.addEventListener('click', function () {
-                        self.visible = false;
-                        // self.$emit('update:visible', false)
-                        document.body.removeChild(div);
-                    })
-                }
-            },
-            close() {
-                this.visible = false
-            }
-        },
-        computed: {
-            nativeTrigger() {
-                return this.trigger !== 'right-click' ? this.trigger : 'manual';
-            }
-        }
+export default {
+  name: "PopMenu",
+  props: {
+    trigger: {
+      type: String,
+      default: () => 'click',
+      validator: value => ['click', 'focus', 'hover', 'right-click'].indexOf(value) > -1
+    },
+    placement: String,
+    // visible: {
+    //   type: Boolean,
+    //   default: () => false
+    // }
+  },
+  data() {
+    return {
+      visible: false
     }
+  },
+  methods: {
+    rightClickHander(ev) {
+      let self = this;
+      if (self.trigger === 'right-click') {
+        ev.preventDefault();
+        self.visible = true;
+        // self.$emit('update:visible', true)
+        let div = document.createElement('div');
+        div.style = 'position: absolute; width: 100%; height: 100%; top: 0; left: 0; z-index: 1;';
+        document.body.appendChild(div);
+        div.addEventListener('click', function () {
+          self.visible = false;
+          // self.$emit('update:visible', false)
+          document.body.removeChild(div);
+        })
+      }
+    },
+    close() {
+      this.visible = false
+    }
+  },
+  computed: {
+    nativeTrigger() {
+      return this.trigger !== 'right-click' ? this.trigger : 'manual';
+    }
+  }
+}
 </script>
 
 <style scoped>
