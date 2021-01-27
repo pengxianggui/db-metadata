816501c30d527dd47e1db4f5ddab6b3ac9a7c028
pengxianggui <pengxianggui@outlook.com>
2021-01-27 16:09:37 +0800

    基于jfinal-spring-boot-starter封装一个db-meta-spring-boot-starter。dbmeta配置文件支持在spring配置文件中声明。采用spring的方式进行dbmeta扩展

---
+++ db-metadata-spring-boot-starter/src/main/java/com/dbmeta/starter/config/ComponentConfig.java
@@ -0,0 +1,38 @@
+package com.dbmeta.starter.config;
+
+import com.hthjsj.AnalysisManager;
+import com.hthjsj.analysis.component.ComponentType;
+import com.hthjsj.web.AppConst;
+import com.hthjsj.web.ExtJFinalConfig;
+import com.hthjsj.web.component.Components;
+import com.hthjsj.web.kit.InitKit;
+import com.hthjsj.web.ui.ComputeKit;
+import com.hthjsj.web.ui.meta.CCUUConfigExtension;
+import com.hthjsj.web.ui.meta.InstanceConfigExtension;
+import com.hthjsj.web.ui.meta.MetaFieldConfigExtension;
+
+/**
+ * @author pengxg
+ * 2021/1/27 2:13 下午
+ */
+public class ComponentConfig extends ExtJFinalConfig {
+
+    @Override
+    public void onStart() {
+        if (AppConst.getProp().getBoolean(AppConst.COMPONENT_ALLOW_REPLACE)) {
+            //component register
+            Components.me().init();
+            Components.me()
+                    .addAutoInitComponents(ComponentType.SEARCHVIEW)
+                    .addAutoInitComponents(ComponentType.TABLEVIEW)
+                    .addAutoInitComponents(ComponentType.FORMVIEW);
+        }
+        //Auto import anyConfig from json file;
+        if (AppConst.getProp().getBoolean(AppConst.CONFIG_ALLOW_REPLACE)) {
+            InitKit.me().importMetaObjectConfig().importInstanceConfig();
+        }
+        AnalysisManager.me().addMetaFieldConfigExtension(new MetaFieldConfigExtension());
+        ComputeKit.addInstanceExtension(new InstanceConfigExtension());
+        ComputeKit.addInstanceExtension(new CCUUConfigExtension());
+    }
+}
