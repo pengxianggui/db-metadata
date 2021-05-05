4af1b364abf808dd73cf05e6d0d726d083b195a2
pengxianggui <pengxianggui@outlook.com>
2021-05-05 15:19:57 +0800

    1. 移除无用的打印

--- db-metadata-web/package/svg/index.js
+++ db-metadata-web/package/svg/index.js
@@ -1,6 +1,5 @@
 const req = require.context('./src', true, /\.svg$/)
 const requireAll = requireContext => {
-    console.log(requireContext)
     return requireContext.keys().map(requireContext)
 }
 requireAll(req)
