c0f1940bfb830ef01b11663f84f93af6301f4a84
konbluesky <konbluesky@163.com>
2021-09-08 11:33:20 +0800

    polish anything;

--- db-metadata-analysis-springboot/src/main/java/com/hthjsj/analysis/db/SnowFlake.java
+++ db-metadata-analysis-springboot/src/main/java/com/hthjsj/analysis/db/SnowFlake.java
@@ -65,17 +65,6 @@ public class SnowFlake {
         return me;
     }
 
-    public static void main(String[] args) {
-        SnowFlake snowFlake = new SnowFlake(1, 1);
-
-        long start = System.currentTimeMillis();
-        for (int i = 0; i < 1000000; i++) {
-            System.out.println(snowFlake.nextId());
-        }
-
-        System.out.println(System.currentTimeMillis() - start);
-    }
-
     /**
      * 产生下一个ID
      *
