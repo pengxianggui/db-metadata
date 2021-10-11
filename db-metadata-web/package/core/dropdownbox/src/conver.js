0d5c9a233a9035085f3076a349d1fbd89dc4db41
pengxg <pengxianggui@outlook.com>
2021-10-11 18:02:08 +0800

    fix:修复缺陷

--- db-metadata-web/package/core/dropdownbox/src/conver.js
+++ db-metadata-web/package/core/dropdownbox/src/conver.js
@@ -9,12 +9,15 @@ export default function (value) {
         switch (utils.typeOf(value)) {
             case "[object String]":
                 nativeVal = (value.trim() === '' ? [] : value.split(','));
+                break
             case "[object Number]":
             case "[object Boolean]":
                 tempValue = utils.convertToString(value);
                 nativeVal.push(tempValue);
+                break
             case "[object Array]":
                 nativeVal = value.map(ele => utils.convertToString(ele));
+                break
             default:
         }
         return nativeVal
