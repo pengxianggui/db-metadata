67980867172a1b79cb8b9aba7ff6449e758b1959
pengxg <pengxianggui@outlook.com>
2021-11-02 20:09:47 +0800

    fix: 修复缺陷——JsonParameterToMapHandler中对body入参进行提取后导致request流被读取，业务控制器无法再次获取

--- db-metadata-server-springboot/src/main/java/com/github/md/web/config/json/JsonParameterToMapHandler.java
+++ db-metadata-server-springboot/src/main/java/com/github/md/web/config/json/JsonParameterToMapHandler.java
@@ -10,9 +10,12 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import javax.servlet.ReadListener;
+import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 import java.util.*;
 
 /**
@@ -56,7 +59,8 @@ public class JsonParameterToMapHandler implements HandlerInterceptor {
         return true;
     }
 
-    static class WritableHttpServletRequestWrapper extends HttpServletRequestWrapper {
+    public static class WritableHttpServletRequestWrapper extends HttpServletRequestWrapper {
+        private final String body; // 实现body可重读
 
         Map<String, String[]> params = Maps.newHashMap();
 
@@ -66,9 +70,86 @@ public class JsonParameterToMapHandler implements HandlerInterceptor {
          * @param request the {@link HttpServletRequest} to be wrapped.
          * @throws IllegalArgumentException if the request is null
          */
-        public WritableHttpServletRequestWrapper(HttpServletRequest request) {
+        public WritableHttpServletRequestWrapper(HttpServletRequest request) throws IOException {
             super(request);
+            this.body = parseBody(request); // 转储body
             params.putAll(request.getParameterMap());
+        }
+
+        @Override
+        public ServletInputStream getInputStream() {
+            final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(body.getBytes());
+            return new ServletInputStream() {
+                @Override
+                public boolean isFinished() {
+                    return false;
+                }
+
+                @Override
+                public boolean isReady() {
+                    return false;
+                }
+
+                @Override
+                public void setReadListener(ReadListener readListener) {
+                }
+
+                @Override
+                public int read() {
+                    return byteArrayInputStream.read();
+                }
+            };
+
+        }
+
+        @Override
+        public BufferedReader getReader() {
+            return new BufferedReader(new InputStreamReader(this.getInputStream()));
+        }
+
+        public String getBody() {
+            return this.body;
+        }
+
+        /**
+         * 转储body
+         *
+         * @param request
+         * @throws IOException
+         */
+        private String parseBody(HttpServletRequest request) throws IOException {
+            StringBuilder stringBuilder = new StringBuilder();
+            BufferedReader bufferedReader = null;
+            InputStream inputStream = null;
+            try {
+                inputStream = request.getInputStream();
+                if (inputStream != null) {
+                    bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                    char[] charBuffer = new char[128];
+                    int bytesRead;
+                    while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+                        stringBuilder.append(charBuffer, 0, bytesRead);
+                    }
+                }
+            } catch (IOException e) {
+                throw e;
+            } finally {
+                if (inputStream != null) {
+                    try {
+                        inputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (bufferedReader != null) {
+                    try {
+                        bufferedReader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return stringBuilder.toString();
         }
 
         public void init(Map<String, String> flatParams) {
