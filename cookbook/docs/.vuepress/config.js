7d462f2fe8b44015a737c9ab647530e3f1e535ce
pengxg <pengxianggui@outlook.com>
2022-03-28 10:26:22 +0800

    update: cookbook

--- cookbook/docs/.vuepress/config.js
+++ cookbook/docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 // <img :src="$withBase('/foo.png')" alt="foo"
 
 module.exports = {
-    title: 'Db-Metadata 教程 (v2.1)',
+    title: 'Db-Metadata 教程 (v2.2)',
     description: '手册将介绍Db-Metadata研发初衷、设计思路、核心概念、使用指南和扩展方法',
     head: [
         ['link', {rel: 'shortcut icon', type: 'image/x-icon', href: '/logo.png'}],
