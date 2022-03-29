1a03883cb735211d4123fc6e0ad87af304c8d0e1
pengxg <pengxianggui@outlook.com>
2022-03-29 11:42:15 +0800

    update: 合并镜像仓库

--- build.sh
+++ build.sh
@@ -10,26 +10,26 @@ version=$1
 prefix="registry.cn-hangzhou.aliyuncs.com/hthj_asoco/"
 
 # build server
-docker  build  -f db-metadata-server-demo/Dockerfile  --rm=true  -t $prefix"db-metadata-server:"${version} ./db-metadata-server-demo ;
+docker  build  -f db-metadata-server-demo/Dockerfile  --rm=true  -t $prefix"db-metadata:server-"${version} ./db-metadata-server-demo ;
 
 # build web
-docker  build -f db-metadata-web/Dockerfile --rm=true  -t $prefix"db-metadata-web:"${version} ./db-metadata-web ;
+docker  build -f db-metadata-web/Dockerfile --rm=true  -t $prefix"db-metadata:web-"${version} ./db-metadata-web ;
 
 # build cookbook
-docker  build -f cookbook/Dockerfile --rm=true  -t $prefix"db-metadata-cookbook:"${version} ./cookbook ;
+docker  build -f cookbook/Dockerfile --rm=true  -t $prefix"db-metadata:cookbook-"${version} ./cookbook ;
 
 #登录凭证
 docker login -u "${PLUGIN_DOCKER_USERNAME}" -p "${PLUGIN_DOCKER_PASSWORD}" registry.cn-hangzhou.aliyuncs.com;
 
 # 推送镜像
-docker push $prefix"db-metadata-server:"${version} ;
-docker push $prefix"db-metadata-web:"${version} ;
-docker push $prefix"db-metadata-cookbook:"${version} ;
+docker push $prefix"db-metadata:server-"${version} ;
+docker push $prefix"db-metadata:web-"${version} ;
+docker push $prefix"db-metadata:cookbook-"${version} ;
 
 # 清理镜像
-docker rmi $prefix"db-metadata-server:"${version} ;
-docker rmi $prefix"db-metadata-web:"${version} ;
-docker rmi $prefix"db-metadata-cookbook:"${version} ;
+docker rmi $prefix"db-metadata:server-"${version} ;
+docker rmi $prefix"db-metadata:web-"${version} ;
+docker rmi $prefix"db-metadata:cookbook-"${version} ;
 
 #清理空间
 docker system prune -f;
