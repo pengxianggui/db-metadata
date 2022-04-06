a2b40659221f4b3c73f7bc189dfad114e797891f
pengxg <pengxianggui@outlook.com>
2022-04-06 15:31:34 +0800

    update

--- build.sh
+++ build.sh
@@ -14,29 +14,39 @@ echo "version:${version}"
 echo "prefix:${prefix}"
 
 if [ ${type} == 'cookbook' ]; then
+  echo "构建cookbook镜像..."
   # build cookbook
   docker  build -f cookbook/Dockerfile --rm=true  -t $prefix"cookbook-"${version} ./cookbook ;
 
+  echo "登录镜像仓库..."
   #登录凭证
   docker login -u "${PLUGIN_DOCKER_USERNAME}" -p "${PLUGIN_DOCKER_PASSWORD}" registry.cn-hangzhou.aliyuncs.com;
+  echo "推送镜像..."
   docker push $prefix"cookbook-"${version} ;
+  echo "删除本地镜像..."
   docker rmi $prefix"cookbook-"${version} ;
 else
+  echo "构建server镜像..."
   # build server
   docker  build  -f db-metadata-server-demo/Dockerfile  --rm=true  -t $prefix"server-"${version} ./db-metadata-server-demo ;
 
+  echo "构建web镜像..."
   # build web
   docker  build -f db-metadata-web/Dockerfile --rm=true  -t $prefix"web-"${version} ./db-metadata-web ;
 
-  #登录凭证
+  echo "登录镜像仓库..."
   docker login -u "${PLUGIN_DOCKER_USERNAME}" -p "${PLUGIN_DOCKER_PASSWORD}" registry.cn-hangzhou.aliyuncs.com;
 
-  # 推送镜像
+  echo "推送server镜像..."
   docker push $prefix"server-"${version} ;
+
+  echo "推送web镜像..."
   docker push $prefix"web-"${version} ;
 
   # 清理镜像
+  echo "清理本地server镜像..."
   docker rmi $prefix"server-"${version} ;
+  echo "清理本地web镜像..."
   docker rmi $prefix"web-"${version} ;
 fi
 
