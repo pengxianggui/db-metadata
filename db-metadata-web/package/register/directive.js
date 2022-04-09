93b17813bed1b5ac1bae04af4dc62abcdc5cb342
pengxg <pengxianggui@outlook.com>
2022-04-10 07:40:55 +0800

    fix: 解决权限数据异步时, 按钮权限自定义指令问题;

--- db-metadata-web/package/register/directive.js
+++ db-metadata-web/package/register/directive.js
@@ -1,77 +1,104 @@
 import {hasAnyRole, hasAnyAuth, hasAllRole, hasAllAuth, hasAuth, hasRole} from "../access"
-import {isObject} from "../utils/common";
+import {isObject, isString} from "../utils/common";
 
-// TODO FIXME 2.2 当指令绑定的值是异步获取时，无法做到响应式. 导致每次这里取到的值(binding.value)都是undefined
+/**
+ * `v-authorize`：只接受对象参数类型，参数格式如下：
+ * <pre>
+ *  {
+ *      need_permit: true,
+ *      permit_by: "auth",
+ *      auths: ['authCode1'],
+ *      auth_match_mode: 'any',
+ *      roles: ['roleCode1'],
+ *      role_match_mode: 'any'
+ *  }
+ * </pre>
+ *
+ * 其他指令的参数类型为: 字符串数组 OR 字符串
+ */
 export default function (Vue) {
     const directives = {
         'authorize': {
-            inserted: function (el, binding) {
-                Vue.nextTick(() => {
-                    if (!isObject(binding.value)) {
-                        return
-                    }
-                    const {need_permit = false, permit_by = 'auth'} = binding.value
-                    if (need_permit !== true) {
-                        return
-                    }
+            update: function (el, binding) {
+                const {value, oldValue} = binding
+                if (!isObject(value)) {
+                    return
+                }
 
-                    let permit;
-                    if (permit_by === 'auth') {
-                        const {auths = [], auth_match_mode = 'any'} = binding.value
-                        permit = hasAuth(auths, auth_match_mode)
-                    } else if (permit_by === 'role') {
-                        const {roles = [], role_match_mode = 'any'} = binding.value
-                        permit = hasRole(roles, role_match_mode)
-                    }
+                if (JSON.stringify(value) == JSON.stringify(oldValue)) {
+                    return
+                }
 
-                    if (permit === false) { // 无权限，则移除DOM
-                        el.parentNode.removeChild(el)
-                    }
-                })
+                const {need_permit = false, permit_by = 'auth'} = binding.value
+                if (need_permit !== true) {
+                    return
+                }
+
+                let permit;
+                if (permit_by === 'auth') {
+                    const {auths = [], auth_match_mode = 'any'} = binding.value
+                    permit = hasAuth(auths, auth_match_mode)
+                } else if (permit_by === 'role') {
+                    const {roles = [], role_match_mode = 'any'} = binding.value
+                    permit = hasRole(roles, role_match_mode)
+                }
+
+                if (permit === false) { // 无权限，则移除DOM
+                    el.parentNode.removeChild(el)
+                }
             }
         },
         'any-roles': {
             inserted: function (el, binding) {
-                Vue.nextTick(() => {
-                    if (!hasAnyRole(binding.value)) {
-                        if (el.parentNode) {
-                            el.parentNode.removeChild(el)
-                        }
+                let {value = []} = binding
+                if (isString(value)) {
+                    value = [value]
+                }
+
+                if (!hasAnyRole(value)) {
+                    if (el.parentNode) {
+                        el.parentNode.removeChild(el)
                     }
-                })
+                }
             }
         },
         'any-auths': {
             inserted: function (el, binding) {
-                if (!hasAnyAuth(binding.value)) {
-                    Vue.nextTick(() => {
-                        if (el.parentNode) {
-                            el.parentNode.removeChild(el)
-                        }
-                    })
+                let {value = []} = binding
+                if (isString(value)) {
+                    value = [value]
+                }
+                if (!hasAnyAuth(value)) {
+                    if (el.parentNode) {
+                        el.parentNode.removeChild(el)
+                    }
                 }
             }
         },
         'all-roles': {
             inserted: function (el, binding) {
-                Vue.nextTick(() => {
-                    if (!hasAllRole(binding.value)) {
-                        if (el.parentNode) {
-                            el.parentNode.removeChild(el)
-                        }
+                let {value = []} = binding
+                if (isString(value)) {
+                    value = [value]
+                }
+                if (!hasAllRole(value)) {
+                    if (el.parentNode) {
+                        el.parentNode.removeChild(el)
                     }
-                })
+                }
             }
         },
         'all-auths': {
             inserted: function (el, binding) {
-                Vue.nextTick(() => {
-                    if (!hasAllAuth(binding.value)) {
-                        if (el.parentNode) {
-                            el.parentNode.removeChild(el)
-                        }
+                let {value = []} = binding
+                if (isString(value)) {
+                    value = [value]
+                }
+                if (!hasAllAuth(value)) {
+                    if (el.parentNode) {
+                        el.parentNode.removeChild(el)
                     }
-                })
+                }
             }
         }
     }
