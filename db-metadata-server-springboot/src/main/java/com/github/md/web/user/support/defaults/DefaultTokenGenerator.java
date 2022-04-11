15e1ef7a55a3e74154b811fb75652b69a2f52f10
pengxg <pengxianggui@outlook.com>
2022-04-11 10:08:16 +0800

    fix: 参数问题;

--- db-metadata-server-springboot/src/main/java/com/github/md/web/user/support/defaults/DefaultTokenGenerator.java
+++ db-metadata-server-springboot/src/main/java/com/github/md/web/user/support/defaults/DefaultTokenGenerator.java
@@ -2,7 +2,7 @@ package com.github.md.web.user.support.defaults;
 
 import cn.hutool.crypto.SecureUtil;
 import com.github.md.web.user.TokenGenerator;
-import com.github.md.web.user.role.DefaultUserWithRoles;
+import com.github.md.web.user.role.UserWithRolesWrapper;
 
 /**
  * 默认的Token生成仅对用户id进行md5加密。
@@ -10,10 +10,10 @@ import com.github.md.web.user.role.DefaultUserWithRoles;
  * @author pengxg
  * @date 2022/4/10 2:39 下午
  */
-public class DefaultTokenGenerator implements TokenGenerator<DefaultUserWithRoles> {
+public class DefaultTokenGenerator implements TokenGenerator<UserWithRolesWrapper> {
 
     @Override
-    public String generate(DefaultUserWithRoles user) {
+    public String generate(UserWithRolesWrapper user) {
         return SecureUtil.md5(user.userId());
     }
 }
--- db-metadata-web/package/meta/feature/FeatureList.vue
+++ db-metadata-web/package/meta/feature/FeatureList.vue
@@ -7,6 +7,13 @@
       <template #edit-btn="{conf, scope}">
         <el-button v-bind="conf" @click="openUpdate(scope.row)"></el-button>
       </template>
+
+      <template #float-right-btn="scope">
+        <div>
+          <el-button size="mini" type="primary" icon="el-icon-download" plain @click="exportData(scope)">导出</el-button>
+          <el-button size="mini" type="success" icon="el-icon-upload" plain @click="importData(scope)">导入</el-button>
+        </div>
+      </template>
     </single-grid-tmpl>
     <el-dialog :visible.sync="visible" width="80%" :destroy-on-close="true">
       <feature-add @ok="handleOk" @cancel="visible = false" v-if="primary === null"></feature-add>
@@ -42,6 +49,15 @@ export default {
     handleOk() {
       this.visible = false
       this.$refs[this.fc].refresh()
+    },
+    exportData(scope) {
+      // TODO 2.3 导入整个功能(连同 功能配置 + UI实例配置 + 元对象 + 元字段 + 菜单 + 路由)， 导出格式： 1.SQL; 2.JSON格式(dbmeta需提供配套的导入)
+      //  如果需要连同菜单、路由一起导出的话，那么功能和菜单、路由之间的关联必须再"硬"一点了，而不能仅仅依靠路径后面的?fc={fc}了
+      this.$message.warning('努力开发中...')
+    },
+    importData() {
+      // TODO 2.3 导入功能(json)
+      this.$message.warning('努力开发中...')
     }
   }
 }
